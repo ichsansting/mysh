e3b024433bb65e5d11be037552e6acd04b74233d
Ichsan Sting <139094570+ichsansting@users.noreply.github.com>
2026-08-24 03:37:24 +0000

    Update

--- profile/.config/fish/conf.d/zellij.fish
+++ profile/.config/fish/conf.d/zellij.fish
@@ -1,4 +1,9 @@
 if test "$TERM" != dumb
-    set -gx ZELLIJ_AUTO_ATTACH true
-    eval (zellij setup --generate-auto-start fish | string collect)
+    # set -gx ZELLIJ_AUTO_ATTACH true
+    # set -gx ZELLIJ_AUTO_EXIT true
+    # eval (zellij setup --generate-auto-start fish | string collect)
+
+    if status is-interactive; and not set -q ZELLIJ
+        exec zellij attach -c main
+    end
 end
