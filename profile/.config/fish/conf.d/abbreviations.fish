7a1c09687c3edfde3734f39f46dae7d25c05ce74
Ichsan Sting <139094570+ichsansting@users.noreply.github.com>
2026-08-28 04:18:30 +0000

    mysh save

--- profile/.config/fish/conf.d/abbreviations.fish
+++ profile/.config/fish/conf.d/abbreviations.fish
@@ -13,7 +13,6 @@ abbr -a diff delta
 abbr -a claudej 'BUN_JSC_useJIT=0 claude' # JIT crash workaround
 abbr -a gst 'git status'
 abbr -a glg 'git log --oneline --graph --decorate --all'
-abbr -a gco 'git checkout'
 abbr -a gbr 'git branch'
 abbr -a gunstage 'git reset HEAD --'
 abbr -a glast 'git log -1 HEAD'
