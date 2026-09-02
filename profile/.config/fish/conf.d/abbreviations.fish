2262c064fda4ac98e6b6aa11f2c80d1266b94e5a
Ichsan Sting <139094570+ichsansting@users.noreply.github.com>
2026-09-02 04:32:43 +0000

    mysh save

--- profile/.config/fish/conf.d/abbreviations.fish
+++ profile/.config/fish/conf.d/abbreviations.fish
@@ -7,7 +7,7 @@ abbr -a grep rg
 abbr -a find fd
 abbr -a top btop
 abbr -a htop btop
-abbr -a du dust
+abbr -a du 'dust -d1 -n 10'
 abbr -a df duf
 abbr -a diff delta
 abbr -a claudej 'BUN_JSC_useJIT=0 claude' # JIT crash workaround
